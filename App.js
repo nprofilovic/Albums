fc51414f544f85f40dd20bc5aed88ded25c3612b
nprofilovic <nprofilovic@gmail.com>
2019-07-13 21:13:31 +0200

    Add Header Component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Header title={"Albums"} />
     </View>
   );
 }
